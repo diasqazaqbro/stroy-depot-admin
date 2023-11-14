3be73361fb1d2a02cc014c072548898c4daca38e
diasqazaqbro <diasqazaqbro@mail.ru>
2023-11-14 19:45:04 +0600

    fix

--- pages/api/settings.js
+++ pages/api/settings.js
@@ -6,13 +6,17 @@ export default async function handle(req, res) {
   await mongooseConnect();
 
   if (method === "GET") {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Замените на ваш домен
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, PUT, POST, DELETE, OPTIONS"
+    );
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     res.json(await Settings.find());
   }
 
   if (method === "PUT") {
-    const {
-      number,
-    } = req.body;
+    const { number } = req.body;
     await Settings.updateOne({
       number,
     });
