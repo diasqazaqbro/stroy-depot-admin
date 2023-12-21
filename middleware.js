4c0a2b2c1bf5590a02f92289e017f94587267023
diasqazaqbro <diasqazaqbro@mail.ru>
2023-12-21 16:05:25 +0600

    remove middleware

--- middleware.js
+++
@@ -1,9 +0,0 @@
-import NextAuthMiddleware from 'next-auth/middleware';
-import { setCorsHeaders } from './lib/cors';
-
-export default async function handle(req, res) {
-  setCorsHeaders(req, res, () => {});
-
-  await NextAuthMiddleware(req, res);
-
-}
