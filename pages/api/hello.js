ff69bafef5700d9b7c69262883120dbf37928e6d
diasqazaqbro <diasqazaqbro@mail.ru>
2023-12-21 15:33:20 +0600

    fix cors

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+	res.setHeader("Access-Control-Allow-Origin", "https://hudos.kz"); // Specify your actual domain
+	res.setHeader(
+	  "Access-Control-Allow-Methods",
+	  "GET, PUT, POST, DELETE, OPTIONS"
+	);
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 	res.status(200).json({ name: 'Dias' })
 }
