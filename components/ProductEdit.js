bcf6bc968fd8daf968e7ae3a5c8d13cdb357229c
diasqazaqbro <diasqazaqbro@mail.ru>
2024-02-29 18:30:06 +0600

    new feat

--- components/ProductEdit.js
+++ components/ProductEdit.js
@@ -1,14 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
+import { Editor } from "@tinymce/tinymce-react";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { storage } from "@/lib/config";
 
 export default function ProductEdit({ id }) {
+  const [file, setFile] = useState();
+  const editorRef = useRef(null);
   useEffect(() => {
     axios.get(`/api/products?id=${id}`).then((response) => {
       setData(response.data);
     });
   }, [id]);
   const [data, setData] = useState({});
+  const [isChecked, setIsChecked] = useState(data.importantProducts);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setData((prevData) => ({
@@ -21,20 +28,54 @@ export default function ProductEdit({ id }) {
   const router = useRouter();
 
   async function saveProduct(ev) {
-    ev.preventDefault();
-    console.log(data);
-    await axios.put("/api/products", { ...data, id });
-    setGoToProducts(true);
+    if (editorRef.current) {
+      ev.preventDefault();
+      const storageRef = ref(storage, `products/${title}`);
+      await uploadBytes(storageRef, file);
+      const photoURL = await getDownloadURL(storageRef);
+      const postData = {
+        code: data.code,
+        description: editorRef.current.getContent(),
+        price: data.price,
+        title: data.title,
+        image: photoURL,
+        importantProducts: isChecked,
+        category: data.category,
+      };
+      await axios.put("/api/products", { ...postData, id });
+      setGoToProducts(true);
+    }
   }
 
   if (goToProducts) {
     router.push("/products");
   }
 
+  const handleCheckedChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
   return (
     <>
       <form onSubmit={saveProduct}>
         <div className="form-group">
+          <label htmlFor="fileInput" className="cursor-pointer">
+            <span className="bg-blue-500 text-white px-2 mr-4 py-1 rounded">
+              Добавить фотографию
+            </span>
+            <input
+              id="fileInput"
+              onClick={(event) => {
+                handleFileChange(event);
+              }}
+              type="file"
+              style={{ display: "none" }}
+            />
+          </label>
           <div className="form-group">
             <label htmlFor="title">Заголовок</label>
             <input
@@ -46,6 +87,22 @@ export default function ProductEdit({ id }) {
               onChange={handleChange}
             />
           </div>
+          <select
+            id="category"
+            name="category"
+            value={data.category}
+            onChange={handleChange}
+            className="p-2 border rounded-md"
+          >
+            <option value={1}>Полиуретановые пены</option>
+            <option value={2}>Пено клей</option>
+            <option value={3}>Силиконовые герметики</option>
+            <option value={4}>Монтажные клей</option>
+            <option value={5}>Краски аэрозольные</option>
+            <option value={6}>Затирка для швов</option>
+            <option value={7}>Обойные клей</option>
+            <option value={8}>Разное</option>
+          </select>
           <label htmlFor="price">Цена</label>
           <input
             type="text"
@@ -67,29 +124,55 @@ export default function ProductEdit({ id }) {
             onChange={handleChange}
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="image">Ссылка фотка</label>
-          <input
-            type="text"
-            id="image"
-            name="image"
-            className="form-control"
-            value={data.image || ""}
-            onChange={handleChange}
-          />
+        <div className="mt-4 mb-6">
+          <div className="flex self-center">
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={handleCheckedChange}
+              className="mr-2 h-5 w-5 text-blue-500 focus:ring-blue-400 border-gray-300 rounded"
+            />
+            <span className="text-sm font-medium text-gray-900">
+              Главный товар
+            </span>
+          </div>
         </div>
-
-        <div className="form-group">
-          <label htmlFor="description">Описание</label>
-          <input
-            type="text"
-            id="description"
-            name="description"
-            className="form-control"
-            value={data.description || ""}
-            onChange={handleChange}
-          />
-        </div>
+        <Editor
+          apiKey="a3mu452cbh2dxc6zy2w75jelhgvjq4nk8ic27hcwfkx12mhv"
+          onInit={(evt, editor) => (editorRef.current = editor)}
+          initialValue={data.description}
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              "advlist",
+              "autolink",
+              "lists",
+              "link",
+              "image",
+              "charmap",
+              "preview",
+              "searchreplace",
+              "visualblocks",
+              "codesample code",
+              "fullscreen",
+              "insertdatetime",
+              "media",
+              "table",
+              "code",
+              "help",
+              "wordcount",
+            ],
+            toolbar:
+              "undo redo | blocks | " +
+              "bold italic forecolor | alignleft aligncenter " +
+              "alignright alignjustify | bullist numlist outdent indent |" +
+              "media | image  | preview | table | code",
+            content_style:
+              "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          }}
+        />
+        <img src={data.image} className="h-[400px] pt-5 border-t-2" />
 
         <button type="submit" className="btn-primary my-3">
           Сохранить
--- components/ProductsForm.js
+++ components/ProductsForm.js
@@ -1,45 +1,48 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useRouter } from "next/router";
 import { addDoc, collection } from "firebase/firestore/lite";
 import { db, storage } from "@/lib/config";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { Editor } from "@tinymce/tinymce-react";
 
 export default function ProductForm() {
   const [title, setTitle] = useState();
   const [code, setCode] = useState();
-  const [description, setDescription] = useState();
   const [price, setPrice] = useState();
   const [goToProducts, setGoToProducts] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
   const [file, setFile] = useState();
+  const editorRef = useRef(null);
   const [selectedCategory, setSelectedCategory] = useState(1);
   const router = useRouter();
 
   async function saveProduct(ev) {
-    ev.preventDefault();
-    if (!file) {
-      alert("Загрузите фотку");
-      return;
+    if (editorRef.current) {
+      ev.preventDefault();
+      if (!file) {
+        alert("Загрузите фотку");
+        return;
+      }
+
+      const productsCollection = collection(db, "products");
+      const currentDate = new Date();
+
+      const storageRef = ref(storage, `products/${title}`);
+      await uploadBytes(storageRef, file);
+      const photoURL = await getDownloadURL(storageRef);
+      const updatedData = {
+        code: code,
+        description: editorRef.current.getContent(),
+        price: price,
+        title: title,
+        image: photoURL,
+        time_posted: currentDate,
+        importasntProducts: isChecked,
+        category: selectedCategory,
+      };
+      await addDoc(productsCollection, updatedData);
+      setGoToProducts(true);
     }
-
-    const productsCollection = collection(db, "products");
-    const currentDate = new Date();
-
-    const storageRef = ref(storage, `products/${title}`);
-    await uploadBytes(storageRef, file);
-    const photoURL = await getDownloadURL(storageRef);
-    const updatedData = {
-      code: code,
-      description: description,
-      price: price,
-      title: title,
-      image: photoURL,
-      time_posted: currentDate,
-      importantProducts: isChecked,
-      category: selectedCategory
-    };
-    await addDoc(productsCollection, updatedData);
-    setGoToProducts(true);
   }
 
   if (goToProducts) {
@@ -123,11 +126,40 @@ export default function ProductForm() {
           value={price}
           onChange={(ev) => setPrice(ev.target.value)}
         />
-        <label>Описание</label>
-        <textarea
-          placeholder="Описание"
-          value={description}
-          onChange={(ev) => setDescription(ev.target.value)}
+        <Editor
+          apiKey="a3mu452cbh2dxc6zy2w75jelhgvjq4nk8ic27hcwfkx12mhv"
+          onInit={(evt, editor) => (editorRef.current = editor)}
+          initialValue={'<h2>Применение</h2><p>&nbsp;</p><h2><span style="color: rgb(0, 0, 0);">Преимущества</span></h2><p>&nbsp;</p><h2><span style="color: rgb(0, 0, 0);">Технические характеристики</span></h2><table style="border-collapse: collapse; width: 97.8405%; height: 176.344px;" border="1"><colgroup><col style="width: 50.0003%;"><col style="width: 50.0003%;"></colgroup><tbody><tr style="height: 19.5938px;"><td style="height: 19.5938px;">Высокая производительность: до 65 литров</td><td style="height: 19.5938px;">&nbsp;</td></tr><tr style="height: 19.5938px;"><td style="height: 19.5938px;">Предварительное время обработки:</td><td style="height: 19.5938px;">&nbsp;</td></tr><tr style="height: 19.5938px;"><td style="height: 19.5938px;">Температура баллона (оптимально +20&deg;C):</td><td style="height: 19.5938px;">&nbsp;</td></tr><tr style="height: 19.5938px;"><td style="height: 19.5938px;">Температура поверхности:</td><td style="height: 19.5938px;">&nbsp;</td></tr><tr style="height: 19.5938px;"><td style="height: 19.5938px;">Класс огнестойкости:</td><td style="height: 19.5938px;">&nbsp;</td></tr><tr style="height: 19.5938px;"><td style="height: 19.5938px;">Срок годности:</td><td style="height: 19.5938px;">&nbsp;</td></tr><tr style="height: 19.5938px;"><td style="height: 19.5938px;">Вид пены:</td><td style="height: 19.5938px;">&nbsp;</td></tr><tr style="height: 19.5938px;"><td style="height: 19.5938px;">Сезон применения:</td><td style="height: 19.5938px;">&nbsp;</td></tr></tbody></table>'}
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              "advlist",
+              "autolink",
+              "lists",
+              "link",
+              "image",
+              "charmap",
+              "preview",
+              "searchreplace",
+              "visualblocks",
+              "codesample code",
+              "fullscreen",
+              "insertdatetime",
+              "media",
+              "table",
+              "code",
+              "help",
+              "wordcount",
+            ],
+            toolbar:
+              "undo redo | blocks | " +
+              "bold italic forecolor | alignleft aligncenter " +
+              "alignright alignjustify | bullist numlist outdent indent |" +
+              "media | image  | preview | table | code",
+            content_style:
+              "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          }}
         />
 
         <button type="submit" className="btn-primary">
